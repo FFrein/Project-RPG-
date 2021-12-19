24d5b73cff50d17d0902280e6c7160391cc59a4c
Ilya <66868765+Svendell@users.noreply.github.com>
2021-12-19 20:07:33 +0300

    RPG-Store
    Магазин. 19.12.2021. Решить проблему с переходом переменных из функции в функцию, если она возникнет.

---
+++ Store.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+void Store( int* hero_money, int* hero_health, int* hero_agility, int* hero_intellect, int* hero_poison) /// Разобраться с переходом переменныъ из одной функции в другую
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "--------------------------------------------------------";
+	cout <<	 "Приветствую тебя, странник. Что желаешь приобрести? ";
+	cout << "--------------------------------------------------------";
+	cout << "МАГАЗИН";
+	cout << "1. Книга увеличения здоровья - цена 10 монет  2.Книга увеличения ловкости - цента 10 монет "<<endl;
+
+	cout << "3.Книга увеличения интеллекта - цена 10 монет 4.Зелье восстановления здоровья - цена 5 монет" << endl;
+
+	cout << "5. Выход из магазина" << endl;
+
+	int action_num; ///Номера лота в магазите
+
+	/// Цикл покупки и выхода из магазина
+
+	do {
+		cin >> action_num;
+		if (action_num == 1) {
+			if ( hero_money > 10) {
+				hero_health = hero_health + 10; // 10 - увеличили здоровье на 10 едениц // МОЖНО МЕНЯТЬ НА ЧИСЛО, КОТОРОЕ ВАМ НУЖНО
+				hero_money = hero_money - 10;
+			}
+			else{
+				cout << "У вас недостаточно денег" << endl;
+			}
+		};
+
+		if (action_num == 2) {
+			if (hero_money > 10) {
+				hero_agility = hero_agility + 10;
+				hero_money = hero_money - 10;
+			}
+			else {
+				cout << "У вас недостаточно денег" << endl;
+			}
+		};
+
+		if (action_num == 3) {
+			if (hero_money > 10) {
+				hero_intellect = hero_intellect + 10;
+				hero_money = hero_money - 10;
+			}
+			else {
+				cout << "У вас недостаточно денег" << endl;
+			}
+		};
+
+		if (action_num == 4) {
+			if (hero_money > 10) {
+				hero_money = hero_money - 10;
+				hero_poison = hero_poison + 1;
+			}
+			else {
+				cout << "У вас недостаточно денег" << endl;
+			}
+		};
+
+		if (action_num == 5) {
+			//Написать функцию выхода в лагерь / меню Никиты
+
+		};
+		cout << "1. Книга увеличения здоровья - цена 10 монет  2.Книга увеличения ловкости - цента 10 монет " << endl;
+
+		cout << "3.Книга увеличения интеллекта - цена 10 монет 4.Зелье восстановления здоровья - цена 5 монет" << endl;
+
+		cout << "5. Выход из магазина" << endl;
+	} while (action_num != 5);
+
+}
