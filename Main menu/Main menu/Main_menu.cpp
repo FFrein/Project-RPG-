f9d50fabee695d3d3da221e727cc31f6a7760e6e
Nikita Kuteyko <nikitaku3366@gmail.com>
2021-12-21 10:08:57 +0300

    Modified main menu

---
+++ Main menu/Main menu/Main_menu.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include<string>
+#include<conio.h>
+#include <math.h>
+#include<Windows.h>
+#include "ClassSelection.h"
+#include "Shop.h"
+
+string ch;
+
+using namespace std;
+void statsHero();
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RUS");
+	ClassSelection();
+choosing:
+	statsHero();
+	cout << "Сделай выбор: ";
+	cin >> ch;
+	switch (ch[0])
+	{
+	case's':
+		system("cls");
+		Store();//магазин
+	case'b':
+		system("cls");
+		//Battle();//бой
+	case'j':
+		system("cls");
+		//Journey();//путешествие
+	default:
+		system("cls");
+		goto choosing;
+	}
+}
+
+
+void statsHero() {
+	cout << "---------------------------------------------Лагерь----------------------------------------------" << endl;
+	cout<<hero_type_name << " " << hero_name << "         уровень : " << hero_level << endl;
+	cout << "Здоровье: " << hero_health << endl;
+	cout << "Ловкость: " << hero_agility << endl;
+	cout << "Интелект: " << hero_intellect << endl;
+	cout << "Зелья: " << hero_poison << endl << endl;
+	cout << "Золото: " << hero_money << endl;
+	cout << "------------ s - shop ---------------- b - battle -------------- j - journey --------------------" << endl;
+}
+
+
+
+
+
+ 
